03c5d60f834332dbb57da9f09cc9ac314d5a5e61
Jess Williams <jesswilliams@Jesss-MacBook-Pro.local>
2019-07-18 09:21:18 -0400

    Add additional columns to base models for stg_customers, stg_order_items, and stg_orders.

--- models/example/stg_order_items.sql
+++ models/example/stg_order_items.sql
@@ -1,1 +1,21 @@
-select id as order_items_id from {{ ref('order_items_upload') }}
+with source as (
+
+    select * from {{ ref('order_items_upload') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_items_upload_id,
+        order_id,
+        price,
+        quantity,
+        size,
+        color,
+        product_id
+    from source
+
+)
+
+select * from renamed
